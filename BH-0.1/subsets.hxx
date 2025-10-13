572f8a53625184787b53823bbf9b8da269c0a7c1
Edgar Gonzàlez i Pellicer <edgar.gip@gmail.com>
2025-10-13 09:05:42 +0200

    Addition of GPL-3 license.

--- BH-0.1/subsets.hxx
+++ BH-0.1/subsets.hxx
@@ -1,3 +1,20 @@
+// hyp-test: Non-parametric hypothesis testing.
+// Copyright (C) 2006-2012  Ignasi Abío i Roig <ignasi.abio@gmail.com>
+//                          Edgar Gonzàlez i Pellicer <edgar.gip@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef SUBSETS_HXX
 #define SUBSETS_HXX
 
